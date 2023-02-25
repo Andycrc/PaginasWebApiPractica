785b50ac3612e92c586c8c03a926a955dbc04be0
ander <ander@DESKTOP-PABPQSI>
2023-02-25 12:57:52 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto) 
+        {
+            _equiposContexto= equiposContexto;
+
+        
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public IActionResult ObtenerEquipos()
+        {
+            List<equipos> ListadoEquipos = (from e in _equiposContexto.equipos
+                                            select e).ToList();
+
+            if (ListadoEquipos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(ListadoEquipos);
+        }
+
+
+    }
+
+   
+
+}
